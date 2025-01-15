6a4b0a2f3a8cb5b5f5e93d7ce7ccca8fcc02ecd3
Thabiso Kgare <Thabisoryan8@gmail.com>
2025-01-15 17:03:17 +0000

    intital

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,58 +1,57 @@
-﻿import 'react';
-import clsx from "clsx";
-import Marker from "./Marker.jsx";
+﻿import clsx from "clsx";
+import { Marker } from "./Marker.jsx";
 
 const Button = ({
                     icon,
-                    children, // Fixed spelling mistake
+                    children,
                     href,
                     containerClassName,
                     onClick,
-                    markerFill
+                    markerFill,
                 }) => {
     const Inner = () => (
         <>
-            <span
-                className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
-                <span className="absolute -left-[1px]">
-                    <Marker markerFill={markerFill}/>
-                </span>
-                {icon && (
-                    <img
-                        src={icon}
-                        alt="circle"
-                        className="size-10 mr-5 object-contain z-10"
-                    />
-                )}
-                <span className="relative z-2 font-poppins base-bold text-p1 uppercase">{children}
-                </span> {/* Fixed spelling mistake */}
-            </span>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
+          <Marker markerFill={markerFill} />
+        </span>
 
-            <span className="glow-before glow-after"/>
+          {icon && (
+              <img
+                  src={icon}
+                  alt="circle"
+                  className="size-10 mr-5 object-contain z-10"
+              />
+          )}
+
+          <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
+      </span>
+
+            <span className="glow-before glow-after" />
         </>
     );
-
     return href ? (
         <a
             className={clsx(
-                'relative p-0.5 g5 rounded-2xl shadow-500', // Removed the comma
-                containerClassName
+                "relative p-0.5 g5 rounded-2xl shadow-500 group",
+                containerClassName,
             )}
             href={href}
         >
-            <Inner/>
+            <Inner />
         </a>
     ) : (
         <button
             className={clsx(
-                'relative p-0.5 g5 rounded-2xl shadow-500', // Removed the comma
-                containerClassName
+                "relative p-0.5 g5 rounded-2xl shadow-500 group",
+                containerClassName,
             )}
             onClick={onClick}
         >
-            <Inner/>
+            <Inner />
         </button>
     );
 };
-
 export default Button;
